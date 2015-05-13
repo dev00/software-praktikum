5d8fa2e63c3e4064724d26241f3f8e5441a916a5
dev00 <michaelrose.1992@gmail.com>
2015-05-13 09:01:08 +0200

    Umstellung auf mysqli und utf8

--- php/pizza_name_sql.php
+++ php/pizza_name_sql.php
@@ -1,11 +1,21 @@
 <?php
-	$verbindung = mysql_connect ("localhost",
-	"root", "")
-	or die ("keine Verbindung moeglich.
-	 Benutzername oder Passwort sind falsch");
-	mysql_select_db("pizzeria")
-	or die ("Die Datenbank existiert nicht.");
+	$mysqli = new mysqli("localhost", "root", "", "pizzeria");
 
-	$abfrage = "SELECT name FROM pizza";
-	$ergebnis = mysql_query($abfrage);
+	/* check connection */
+	if ($mysqli->connect_errno) {
+	    printf("Connect failed: %s\n", $mysqli->connect_error);
+	    exit();
+	}
+if (!$mysqli->set_charset("utf8")) {
+    echo("Error loading character set utf8: %s\n", $mysqli->error);
+}
+	/* Select queries return a resultset */
+	if ($result = $mysqli->query("SELECT name FROM pizza")) {
+	   
+	    while ($row = $result->fetch_object()) {
+			echo "<li><a href=\"$row->name\">$row->name</a></li>";
+	    /* free result set */
+		}
+		$result->close();
+	}
 ?>
--- php/pizza_sql.php
+++ php/pizza_sql.php
@@ -1,13 +1,37 @@
 <?php
-		$verbindung = mysql_connect ("localhost",
-		"root", "")
-		or die ("keine Verbindung moeglich.
-		 Benutzername oder Passwort sind falsch");
+$mysqli = new mysqli("localhost", "root", "", "pizzeria");
 
-		mysql_select_db("pizzeria")
-		or die ("Die Datenbank existiert nicht.");
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+if (!$mysqli->set_charset("utf8")) {
+    printf("Error loading character set utf8: %s\n", $mysqli->error);
+}
+/* Select queries return a resultset */
+if ($result = $mysqli->query("SELECT * FROM pizza")) {
+   
+    while ($row = $result->fetch_object()) {
+	echo "<div class=\"post\">";
+	echo "	<h2 id=\"$row->name\">Pizza $row->name</h2>";
+	echo "	<div class=\"row\">";
+	echo "		<div class=\"col-sm-3\">";
+	echo "			<img src=\"img/$row->name.jpg\" class=\"img-responsive hidden-xs img-rounded\">";
+	echo "		</div>";
+	echo "		<div class=\"col-sm-6\">";
+	echo "			<p>$row->beschreibung</p>";
+	echo "		</div>"	;		
+	echo "		<div class=\"col-sm-3\">";
+	echo "			<p>Preis S: $row->preis_s</p>";
+	echo "			<p>Preis M: $row->preis_m</p>";
+	echo "			<p>Preis L: $row->preis_l</p>";
+	echo "			<p><img src=\"img/bestell.jpg\" class=\"img-responsive\"></p>";
+	echo "		</div>";
+	echo "	</div>";
+	echo "</div>";
 
-
-		$abfrage = "SELECT * FROM pizza";
-		$ergebnis = mysql_query($abfrage);
-?>
+    /* free result set */
+	}
+	$result->close();
+};
