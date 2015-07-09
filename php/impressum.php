e465b0a1702410f80f8364b72ecc7bea83e46d3a
WOPEX <b.sch94@gmail.com>
2015-07-09 18:58:56 +0200

    impressum erstellt

--- php/impressum.php
+++ php/impressum.php
@@ -1,58 +1,64 @@
-      <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="de">
-<head>
+  <head>
     <meta charset="utf-8">
-    <title>Impressum</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-	    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
-	<link href="dist/css/style.css" rel="stylesheet">
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Pizzen</title>
+
+    <!-- Bootstrap -->
+    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="dist/css/style.css" rel="stylesheet">
+    <!-- jQuery -->
+    <script src="dist/js/jquery-2.1.4.min.js"></script>
+    <script src="dist/js/script.js"></script>
+    <script src="dist/js/bootstrap.min.js"></script>
+    <script src="dist/js/jquery-2.1.4.min.js"></script>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-	<script src="dist/js/jquery-1.11.1.min.js"></script>
-	<script src="dist/js/script.js"></script>
-	<script src="dist/js/bootstrap.min.js"></script>
-</head>
-<body> <div class="container">
-		<?php include('navigation.php');?>
+  </head>
+  <body>
+    <?php include('top_nav.php');?>
 
-		<!--Main-->
-		<div class="row content">
-			<!--Section Navigation-->
-			<div class="col-md-2 col-sm-3 col-xs-9 hidden-print sidenav">
-				<nav id="sidenav">
-					<ul>
-						<li><a href="#post1">Post 1</a></li>
-						<li><a href="#post2">Post 2</a></li>
-						<li><a href="#post3">Post 3</a></li>
-					</ul>
-				</nav>
-			</div>
-
-		<!--/Section Navigation-->
-		<!--Site-Content-->
-			<div class="col-md-10 col-sm-9 col-xs-12">
-				<div class="post">
-					<p class="metainfo">am 27.1.2014 von klaus</p>
-					<h2 id="post1">&Uuml;berschrift 1</h2>
-					<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-				</div>
-				<div class="post">
-					<p class="metainfo">am 27.1.2014 von klaus</p>
-					<h2 id="post2">&Uuml;berschrift 2</h2>
-					<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-				</div>
-				<div class="post">
-					<p class="metainfo">am 27.1.2014 von klaus</p>
-					<h2 id="post3">&Uuml;berschrift 3</h2>
-					<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-				</div>
-
-			</div>
-		</div>
-		<!--/Main-->
-	</div>
-</body>? </html>
+    <!-- Row Container -->
+    <div class="container">
+      <!-- Row -->
+      <div class="row">
+        <div class="col-md-12">
+          <p><h1>Impressum</h1></p>
+          
+          <p>
+            <h2>Name und Sitz der Unternehmung:</h2> 
+            <br>
+            Pizzeria Heinrich Hertz GmbH 
+            <br>
+            Südendstraße 51 
+            <br>
+            76135 Karlsruhe 
+          </p>
+          <p>
+            Telefon: 0721 / 1000
+            <br>
+            Fax: 0721 / 1001
+          </p>
+          <p>
+            <h2>Webmaster</h2>
+            <br>
+            Dominic Kis
+            <br> 
+            Michael Rose
+            <br> 
+            Wladislaw Schirschikow
+            <br>
+            Bastian Schott
+          </p>       
+        </div> <!-- /Spalte -->  
+      </div> <!-- /Row -->
+    </div> <!-- /Row Container -->
+  </body>
+</html>
